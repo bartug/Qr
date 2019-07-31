5c579eb4ed69131f463117a7dd7169b7fd176617
bartug <bartugsevindik@gmail.com>
2019-07-31 13:56:49 +0300

    Qr
    .Net ile asp sayfasında Qr yaratma

---
+++ WebApplication3/WebForm1.aspx
@@ -0,0 +1,61 @@
+﻿
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<title>QrCode</title>
+</head> 
+<body>
+    <div class="container">
+        <div class="col-md-4">
+          <h3>ADA MÜHENDİSLİK DOĞALGAZ - GÜLŞEN AKÇELİK</h3>  
+
+  <p>Adres :KURUÇEŞME MAH. 205/3 SOK. NO:10/A BUCA / İZMİR - BUCA</p>
+
+ <p> Faaliyet :PROJE İÇ TESİSAT YAPIM, BAKIM-ONARIM</p>
+
+ <p>Telefon : 453 27 32 - 0507 291 65 64</p> 
+
+  <p>Faks : 454 00 08</p>
+<p>Mail:ada@gmail.com</p>
+  Web Adresi : <asp:Label ID="Label1" runat="server" Text="Label">www.Adamuhendislik.com</asp:Label>
+           
+        </div>
+        <div class="col-md-4">
+            <br />
+            <br />
+            <br />
+            <hr />
+              <form id="form1" runat="server">
+                  <asp:DropDownList style="margin-left:115px;" ID="DropDownList1" runat="server">
+                      <asp:ListItem Value="350" Text="350x350" />
+                      <asp:ListItem Value="500" Text="500x500" />
+                       <asp:ListItem Value="750" Text="750x750" />
+                       <asp:ListItem Value="1000" Text="1000x1000" />
+                  </asp:DropDownList>
+                 <!--<asp:TextBox ID="TextBox1" class="form-control" runat="server"></asp:TextBox>-->
+                  
+            <br />
+            <br />
+            <br />
+       <hr />
+            <asp:Button style="margin-left:130px;" ID="Button1" runat="server" Class="btn btn-success" Text="Button" OnClick="Button1_Click" />
+         
+            <hr />
+       
+           
+         
+    </form>
+        </div>
+        <div class="col-md-4">
+            <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+        </div>
+  
+     </div>
+
+</body>
+</html>
---
+++ WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System.Web.UI.WebControls;
+using QRCoder;
+using System.IO;
+using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+       // string[] email = { "www.bartugsevindik.com", "","www.alıveli.com","www.izmirgaz.com" };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /* cmd2 = new SqlCommand("select [Kolon_Adi_1] from [Tablo_Adi] where [Kolon_Adi_2]=@veri2", cnn);
+            cmd2.Parameters.AddWithValue("@veri2", "değeri");
+            SqlDataReader dr = cmd2.ExecuteReader();
+            if (dr.Read())
+            {
+                textBox1.Text = dr["Kolon_Adi"].ToString(); 
+
+            } */
+            //TextBox1.Text = "";
+           // Label1.Text = TextBox1.Text;
+        }
+
+       
+        public void QrYarat()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(Label1.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            
+            System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+            int x = Int32.Parse(DropDownList1.SelectedItem.Value);
+            imgBarCode.Height = x;
+            imgBarCode.Width = x;
+            using (Bitmap bitMap = qrCode.GetGraphic(20))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byte[] byteImage = ms.ToArray();
+                    imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+                }
+                PlaceHolder1.Controls.Add(imgBarCode);
+            }
+            TextBox1.Text = "";
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           // TextBox1.Text = email[0]; //array[0] => text box ataması ; veri tabanından çekecegim e mailler için deneme
+            QrYarat();
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Qr Kodunuz Yaratıldı!')", true);
+
+        }
+
+        
+    }
+}
